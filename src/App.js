1ed4cc458f93d18d33c40fa3e401f9b1d8f8a7d4
topoescobar <83380940+topoescobar@users.noreply.github.com>
2023-06-27 14:33:53 -0300

    Explicacion tutorial

--- src/App.js
+++ src/App.js
@@ -4,49 +4,67 @@ import { useState, useRef, useEffect } from 'react';
 
 export default function App() {
   //USE STATE: getters y setters
-  const [name, setName] = useState('Persona');
+  const [name, setName] = useState('Juancito');
   const [nameChanged, setNameChanged] = useState(false);
 
   //USE EFFECT: permite controlar que hace en todos los ciclos de vida del componente
   useEffect(() => {
-    //1 montaje o creacion, se ejecuta siempre una vez al inicio
+    //1 mount y update, se ejecuta siempre una vez al inicio
     setNameChanged(!nameChanged);
-    if (name != 'Persona' ) {
-      refButton.current.style.backgroundColor = 'blueviolet'
+    if (name != 'Persona') {
+      refButton.current.style.backgroundColor = 'blueviolet';
     }
-    //2 destruccion
+    //2 unmount - destruccion
     return () => {};
 
     //3 arreglo de dependencias, cuando cambien se ejecuta nuevamente 1
   }, [name]);
 
   //USE REF: para arreglar problemas con el estado
-  const refButton = useRef(null) 
+  const refButton = useRef(null);
 
-
-  const handleClick = () =>{
-    setName('Topo')
-  }
+  const handleClick = () => {
+    setName('Topo');
+  };
 
   const handleRef = () => {
-     refButton.current.click
-  }
- 
+    refButton.current.click;
+  };
+
   return (
     <div>
-      <h1>Hello {name}!</h1>
+      <h1>Usos de hooks en React</h1>
+      <p>Funciones conectadas al ciclo de vida del componente</p>
+      <h2>1. useState</h2>
+      Variable y su setter
+      <p>const [name, setName] = useState('Juancito');</p>
+      <p className="console"> {'<p>Hello {name}! </p>  '} </p>
+      <h3>Hello {name}! </h3>
+      <h2>2. useEffect</h2>
+      <p>
+        Inicializa variables y se ejecuta cada vez que cambia el state de
+        dependencia
+      </p>
       {nameChanged ? (
+        <p>Clickea para cambiar el nombre y esconder este texto</p>
+      ) : (
         <p>
-          CLickea para cambiar el nombre y esconder este texto
+          {' '}
+          Ejecutaste setName (con useState) para cambiar el nombre y se ejecuto
+          el codigo de useEffect por dependencia con la variable name{' '}
         </p>
-      ) : ( <p> Ejecutaste setName y la variable nameChanged con useEffect por dependencia con name </p> ) }
-
+      )}
       {/* con ref el boton ya no deberia andar y se puede ejecutar desde el 2do boton */}
-      <button ref={refButton} onClick={handleClick}> B1 Cambiar nombre</button>
-
+      <button onClick={handleClick} ref={refButton}>
+        {' '}
+        B1 Cambiar nombre
+      </button>
+      <h2>3. useRef </h2>
+      <p>
+        Accede a elementos del DOM directamente, uso en asincronismo, para
+        ejecutar cuando el componente este montado
+      </p>
       <button onClick={handleRef}> Ejecutar B1</button>
-
-
     </div>
   );
 }
